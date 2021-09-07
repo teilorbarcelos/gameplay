b8cce350164c54b67316e50ea7e58017eab2dfa4
Teilor Souza Barcelos <tsb-@live.com>
2021-09-07 11:55:32 -0300

    creating add appointment form

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react"
 import { Text, View } from "react-native"
 import { RectButton } from "react-native-gesture-handler"
+import { Feather } from "@expo/vector-icons"
 
 import { Background } from "../../components/Background"
 import { CategorySelect } from "../../components/CategorySelect"
 import { Header } from "../../components/Header"
 import { styles } from "./styles"
+import { theme } from "../../global/styles/theme"
+
+import { GuildIcon } from "../../components/GuildIcon"
+import { SmallInput } from "../../components/SmallInput"
 
 export function AppointmentCreate() {
     const [category, setCategory] = useState('')
@@ -16,7 +21,12 @@ export function AppointmentCreate() {
                 title="Agendar partida"
             />
 
-            <Text style={styles.label}>
+            <Text
+                style={[
+                    styles.label,
+                    { marginLeft: 24, marginTop: 36, marginBottom: 18 }
+                ]}
+            >
                 Categoria
             </Text>
 
@@ -29,15 +39,51 @@ export function AppointmentCreate() {
             <View style={styles.form}>
                 <RectButton>
                     <View style={styles.select}>
-                        <View style={styles.image} />
+                        {/* <View style={styles.image} /> */}
+                        <GuildIcon />
 
                         <View style={styles.selectBody}>
                             <Text style={styles.label}>
                                 Selecione um servidor
                             </Text>
                         </View>
+                        <Feather
+                            name="chevron-right"
+                            color={theme.colors.heading}
+                            size={18}
+                        />
                     </View>
                 </RectButton>
+
+                <View style={styles.field}>
+                    <View>
+                        <Text style={styles.label}>
+                            Dia e mês
+                        </Text>
+
+                        <View style={styles.column}>
+                            <SmallInput maxLength={2} />
+                            <Text style={styles.divider}>
+                                /
+                            </Text>
+                            <SmallInput maxLength={2} />
+                        </View>
+                    </View>
+
+                    <View>
+                        <Text style={styles.label}>
+                            Hora / minutos
+                        </Text>
+
+                        <View style={styles.column}>
+                            <SmallInput maxLength={2} />
+                            <Text style={styles.divider}>
+                                :
+                            </Text>
+                            <SmallInput maxLength={2} />
+                        </View>
+                    </View>
+                </View>
             </View>
         </Background>
     )
